00fa1a7672f849559f155051176467add5474db1
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-01-12 10:13:04 +0100

    Refactor Angular application structure to use standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterModule, MenuComponent],
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ProyectoDeportesGrupo6';
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  standalone: false,
+  standalone: true,
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
