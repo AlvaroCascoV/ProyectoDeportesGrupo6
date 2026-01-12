1a401630847a83a7837da8f0e6aea9528ceccb89
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-01-12 09:40:31 +0100

    menu,routing and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MenuComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  urlApi: 'https://apideportestajamar.azurewebsites.net/',
+};
