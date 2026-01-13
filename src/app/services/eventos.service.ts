1d544e91a798d798d1c23cb464818b084b1ffa66
alexcl11 <aleecanovaslopez@gmail.com>
2026-01-13 14:15:34 +0100

    Refactor getActividadesPorEvento para simplificar la obtención de actividades por evento y mejorar la legibilidad del código.

--- src/app/services/eventos.service.ts
+++ src/app/services/eventos.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
-import { switchMap, mergeMap, toArray, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Evento } from '../models/Evento';
 import { ActividadesEvento } from '../models/ActividadesEvento';
 
@@ -15,22 +15,17 @@ export class EventosService {
   constructor(private _http : HttpClient) { }
 
   getActividadesPorEvento(): Observable<Evento[]>{
-    // Primero obtenemos la lista de eventos y luego, por cada evento,
-    // hacemos una petición al endpoint que devuelve las actividades de ese evento.
+    // para cada evento sacamos las actividades
+    // metemos las actividades en la propiedad listaActividades de los eventos
     return this._http.get<Evento[]>(this.url + 'api/Eventos').pipe(
-      // convertimos el array de eventos en un stream de eventos
-      switchMap((eventos: Evento[]) =>
-        from(eventos).pipe(
-          // para cada evento, pedimos sus actividades y mapeamos al evento con listaActividades
-          mergeMap((evento: Evento) =>
-            this._http.get<ActividadesEvento[]>(this.url+"api/actividades/actividadesevento/"+evento.idEvento).pipe(
-              map((lista: ActividadesEvento[]) => ({ ...evento, listaActividades: lista }))
-            )
-          ),
-          // recomponemos el array final de eventos ya enriquecidos
-          toArray()
-        )
-      )
+      map((eventos: Evento[]) => {
+        eventos.forEach(e => {
+          this._http.get<ActividadesEvento[]>(`${this.url}api/actividades/actividadeseventos/${e.idEvento}`)
+            .subscribe(lista => e.listaActividades = lista);
+        });
+        // devolvemos los eventos de inmediato; las actividades llegarán luego
+        return eventos;
+      })
     );
   }
 }
