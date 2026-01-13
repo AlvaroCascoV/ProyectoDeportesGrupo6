f6ca0e94697a882618260291547fb62ae04e98ec
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-01-13 18:48:50 +0100

    add eventos page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: '**', redirectTo: '/home' }, // Wildcard route for 404 page
+  { path: 'eventos', component: EventosComponent },
+  { path: '**', redirectTo: '/home' },
 ];
